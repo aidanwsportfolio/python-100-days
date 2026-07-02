04ba953d3573cf1d382e29dcc4ad1fe34ad53ed1
Aidan Scott <aidanscottca@gmail.com>
2026-07-02 03:47:48 -0400

    Completed Day 9 - Blind/Silent Auction

---
+++ Day 9 - Silent Auction/Silent Auction Project.py
@@ -0,0 +1,23 @@
+from art import logo
+print(logo)
+
+def add_to_dictionary():
+    bids = {}  #empty dictionary
+
+    while True:
+        name = input("What's your name? ") #prompt user for name
+        bid = int(input("What's your bid? ")) #prompt user for bid
+
+        # store this player's bid
+        bids[name] = bid #store the players BID, but not the name.
+
+        other_players = input("Are there any other bids? (yes/no) ").lower() #force lowercase for code to work
+        if other_players == "no":
+            break
+        print("\n" * 20) #make console anonymous
+
+    # find highest bid
+    highest_bid = max(bids.values()) #max bid value
+    winner = max(bids) #winning player
+    print(f"The winner is {winner} with ${highest_bid}")
+add_to_dictionary()
